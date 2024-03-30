fdd8ecbf393727e9282ce0a9c0e3f3b6cfa7d8db
1Edum <eduardomatosdesouza2@gmail.com>
2024-03-30 20:51:55 -0300

    completou o Capítulo 12

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -19,7 +19,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   };
   // NOTE: comment in this code when you get to this point in the course
 
-  // const allPages = generatePagination(currentPage, totalPages);
+  const allPages = generatePagination(currentPage, totalPages);
 
   return (
     <>
